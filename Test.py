92c8f2f00856fc004c9ea8023eed1ed264b6f0bb
LiumengSong <liumeng.song@hotmail.com>
2020-01-11 21:58:32 +0000

    Update Test.py
    Add space

--- Test.py
+++ Test.py
@@ -1,20 +1,21 @@
 import unittest
 from TradingGame import Player, Product
 
+
 class TestProduct(unittest.TestCase):
 
     def test_accessAttributes(self):
         testProduct = Product()
-        testProduct.setAttributes(1,1,1,1)
+        testProduct.setAttributes(1, 1, 1, 1)
         self.assertEqual(testProduct.price, 1)
         self.assertEqual(testProduct.terms, 1)
         self.assertEqual(testProduct.revenue, 1)
         self.assertEqual(testProduct.risk, 1)
-        self.assertEqual(testProduct.getAttributes(), (1,1,1,1))
+        self.assertEqual(testProduct.getAttributes(), (1, 1, 1, 1))
 
     def test_accessRoundsRemain(self):
         testProduct = Product()
-        testProduct.setAttributes(1,1,1,1)
+        testProduct.setAttributes(1, 1, 1, 1)
         testProduct.setRoundsRemain()
         self.assertEqual(testProduct.getRoundsRemain(), testProduct.roundsRemain)
         self.assertEqual(testProduct.roundsRemain, 1)
@@ -22,19 +23,20 @@ class TestProduct(unittest.TestCase):
 
     def test_updateRoundsRemain(self):
         testProduct = Product()
-        testProduct.setAttributes(1,1,1,0.4)
+        testProduct.setAttributes(1, 1, 1, 0.4)
         testProduct.updateRoundsRemain(0.2)
         self.assertEqual(testProduct.roundsRemain, -1)
 
         testProduct = Product()
-        testProduct.setAttributes(1,1,1,0.4)
+        testProduct.setAttributes(1, 1, 1, 0.4)
         testProduct.updateRoundsRemain(0.4)
         self.assertEqual(testProduct.roundsRemain, 0)
 
         testProduct = Product()
-        testProduct.setAttributes(1,1,1,0.4)
+        testProduct.setAttributes(1, 1, 1, 0.4)
         testProduct.updateRoundsRemain(0.6)
         self.assertEqual(testProduct.roundsRemain, 0)
+
 
 class TestPlayer(unittest.TestCase):
 
@@ -50,9 +52,10 @@ class TestPlayer(unittest.TestCase):
         dummyPlayer = Player('dummy')
         initMoney = dummyPlayer.money
         testProduct = Product()
-        testProduct.setAttributes(1,1,1,1)
+        testProduct.setAttributes(1, 1, 1, 1)
         dummyPlayer.earnMoney(testProduct)
         self.assertEqual(dummyPlayer.money, initMoney+testProduct.revenue)
+
 
 if __name__ == '__main__':
     unittest.main()
